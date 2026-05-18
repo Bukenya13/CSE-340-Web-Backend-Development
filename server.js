2435c4c91b3d2313ba79cec1c224a4166186eaa0
Bukenya13 <phem.hancho@gmail.com>
2026-05-18 11:36:32 +0300

    iiiiik

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'public', 'src', 'views'));
+app.set('view cache', false);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
